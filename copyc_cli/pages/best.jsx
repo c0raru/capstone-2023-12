17a6981a1a1b457aeca76540dd08b85d3f94eac2
c0raru <coraru@kookmin.ac.kr>
2023-05-20 20:01:43 +0900

    찐완성......

--- copyc_cli/pages/best.jsx
+++ copyc_cli/pages/best.jsx
@@ -1,19 +1,31 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
 import { Button, Dropdown, Grid } from 'semantic-ui-react'
 import { LinkButton } from 'src/components/button'
 import { CardItems } from 'src/components/card'
 import { Card } from 'src/components/card'
 import { LabelMenu } from 'src/components/styles'
 import { ImageBannerContents } from 'src/components/styles'
+import { useProduct } from 'src/hooks/ProductContext'
 import MainLayout from 'src/layouts/MainLayout'
 import styled from 'styled-components'
 
 export default function Best() {
+  const [bestItems, setBestItems] = useState([]);
+  const product = useProduct()
+  useEffect(() => {
+    const page = 1
+    product.search(page)
+    .then(({data}) => {
+      setBestItems(data.results)
+    })
+  }, [])
+
   return (
     <MainLayout category="COPYC">
       <section>
-        <CardItems />
+        <CardItems items={bestItems}/>
       </section>
     </MainLayout>
   )
