f30135f72cc1c5f3739061eaf1c5578dcbcd17dd
c0raru <coraru@kookmin.ac.kr>
2023-05-21 22:00:54 +0900

    완전히 마지막~~~

--- copyc_cli/pages/notice/[id].jsx
+++ copyc_cli/pages/notice/[id].jsx
@@ -1,14 +1,39 @@
-/*
-  @page        : /styles/notice/[id]
-  @description : 공지사항 게시물 상세
-*/
+import axios from 'axios'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 import MainLayout from 'src/layouts/MainLayout'
 import styled from 'styled-components'
 
+const Title = styled.div`
+  font-size: 20px;
+  margin-bottom: 10px;
+  padding-bottom: 10px;
+  border-bottom: 1px solid #9999;
+`
+
+const Content = styled.div`
+  padding: 10px;
+  background-color: #eee;
+`
+
 export default function CustomerNoticeDetail() {
+  const router = useRouter()
+  const [data, setData] = useState({})
+  useEffect(() => {
+    if(!router.isReady) {
+      return;
+    }
+    axios.get("/notice/1").then(({data}) => {
+      setData(data);
+    })
+  }, [router.isReady])
   return (
     <MainLayout category="공지사항">
       <section>
+        <Title>{data.title}</Title>
+        <Content
+          dangerouslySetInnerHTML={{__html: data.contents}}
+        />
       </section>
     </MainLayout>
   )
